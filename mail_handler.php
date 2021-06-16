ffcdab7342846730ba1c2db476f2a2075ce3b06e
Daniel Guzdek <daniel.guzdek.86@gmail.com>
2021-06-16 12:35:32 +0200

    Changed redirect in mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -2,17 +2,26 @@
   if(isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    $subject = $_POST['subject']; 
+	  $message = $_POST['message'];
+    
+    $body_msg = 'From: '.$name."\r\n";
+    $body_msg .= 'Email: '.$email."\r\n";
+    $body_msg .= 'Message: '.$message."\r\n";
 
-    $to = 'daniel.guzdek.86@gmail.com';
-    $headers = 'From: ' .$name. ' <' .$email. '>';
+    $email_server = "dantfnhdfy@s135.cyber-folks.pl";
+	  $title_name = "Form data from www.danny-webdeveloper.pl";
+	
+    $to = 'danielguzdek@vp.pl';
+	
+    $headers = 'From: ' .$title_name. ' <' .$email_server. '>';
     $headers .= 'Content-Type: text/plain; charset=utf-8' . "\r\n";
   }
-  
-  if(mail($to, $subject, $message, $headers)) {
-    echo "<h1>Sent Successfully. Thank you!</h1>";
-  } else {
-    echo "<h1>Something went wrong..</h1>";
-  }
+
+  if (mail($to, $subject, $body_msg, $headers)) { 
+    echo "<script>alert('Thank you for the message. I will contact you shortly.');</script>";
+    echo "<script type='text/javascript'>window.location.href='http://www.danny-webdeveloper.pl/'</script>"; } 
+  else { 
+    echo "<script>alert('The data entered into the form is incorrect. Please try again.');</script>";
+    echo "<script type='text/javascript'>window.location.href='https://www.danny-webdeveloper.pl/pages/contact.html</script>"; }
 ?> 
